82b424d29e17367455e7893d4d8ddaae1d5b3549
Kristina Diamond <krdiamond@gmail.com>
2025-04-20 21:53:24 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <h1 class="text-4xl text-blue-500">Tailwind is working!</h1>
+    <HelloWorld />
+  </div>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="updateMessage">Click Me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      message: 'Hello from Options API!'
+    }
+  },
+  methods: {
+    updateMessage() {
+      this.message = 'You clicked the button!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
