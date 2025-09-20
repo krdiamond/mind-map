9e4241da5dcd60fb54dcc741acd1bd28970235ac
Kristina Diamond <krdiamond@gmail.com>
2025-09-19 23:40:53 -0400

    gold, buddha and airpods are all working

--- src/components/Alchemy.vue
+++ src/components/Alchemy.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
       <div class="close" @click="$emit('close')">X</div>
-        <h2>Alchemy</h2>
-      <p>
-        From 2023 - August 2025 I ran an app on a spare computer that essentially exploited a "social casino".  "Social Casinos" offer USD payouts when users purchase their "social" coins to play slot machines. It is an online gambling loophole. Some casinos offer 1 free coin per a day for simply logging in.  I was running about 4 casinos but only 1 was reliable.  My app would automanically log in every morning, collect the 1 coin and then "wash" it by playing a slot machine. The payouts were coins that were redemable for USD.  I never played the slot machines myself or put any of my own money in.  I made a lot of money doing this.  This was a source of true ◎ Alchemy ◎ ! </p>
-        <p>Then in Aug 2025, NJ freaking passed a new law banning all "simulated gambling" and "social casinos" are now blocked in NJ, NY and all surrounding states so this project is dead.</p> 
-        <p>I won't even get into how complicated it was to make an app that could actually play their slot machines and respond to all the different reactions the machines had.  I had to make sure it responded to everything so that the app wouldn't get stuck.  Also, I had to make sure I could collect my jackpots (there were many) without skipping over them.  It was a tiresome process and my friends and family thought it wasn't worth the money (or they thought I was crazy and didn't want to hear about it) but I live for this shit so <span class="">🤷‍♀️</span>.
-      </p>
-      <img :src="BirdAnimation" alt="Bird animation" class="w-full pt-12" />
+        <h2 class="m-12">Alchemy</h2>
+        <p class="m-12">From the years 2023 to 2025 the Magnum Opus 🜍 was made manifest, revealed in its true form.  Daily the prima materia, freely bestowed, was set into the vessel. By my Stone it was transmuted, washed and perfected, until it shone as gold ☉ and with due solemnity deposited into my checking account in manifold measure. In August 2025, the month of my nativity, New Jersey's rulers forbade the Source, now barred in NJ, NY, and neighboring states. Yet the Work is eternal — TRUE ALCHEMY 🜔 will be discovered again.</p>
+        <h4 class="m-12">꩜ node app - logs into "social casino", captures daily bonus token, plays "Genie" themed slot machine using token and "washes" it into real dollars, depositis dollars into my checking account.  The app no longer works due to NJ laws but here is a preview of my terminal output. </h4>
+        <div class="m-12">
+          <img :src="PassiveBaby" alt="Bird animation" class="w-full" />
+        </div>
+      
       </div>
 </template>
 
 <script setup>
 
-import BirdAnimation from '../assets/bird-animation.svg'
+import PassiveBaby from '../assets/passive-baby.gif'
 
 </script>
--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -4,37 +4,37 @@
         <h2 class="m-12">one unique album per/day</h2>
         <div class="m-12">
           <p class="mb-6">September 2025</p>
-          <table class="album-table">
+          <table>
             <tbody>
               <tr>
                 <td>9.19.2025</td>
                 <td>Codeine - Frigid Stars</td>
-                <td>🤯</td>
+                <td><span class="emoji">😊</span></td>
               </tr>
               <tr>
                 <td>9.18.2025</td>
                 <td>Jets to Brazil - Orange Rhyming Dictionary</td>
-                <td>😐</td>
+                <td><span class="emoji">😐</span></td>
               </tr>
               <tr>
                 <td>9.11.2025</td>
                 <td>Unwound - Repetition</td>
-                <td>😊</td>
+                <td><span class="emoji">😊</span></td>
               </tr>
               <tr>
                 <td>9.10.2025</td>
                 <td>Free Throw - Those Days Are Gone</td>
-                <td>😊</td>
+                <td><span class="emoji">😊</span></td>
               </tr>
               <tr>
                 <td>9.09.2025</td>
                 <td>American Football - Self Titled</td>
-                <td>😐</td>
+                <td><span class="emoji">😐</span></td>
               </tr>
               <tr>
                 <td>9.08.2025</td>
                 <td>Smog - Knock Knock</td>
-                <td>😊</td>
+                <td><span class="emoji">😊</span></td>
               </tr>
             </tbody>
           </table>
@@ -45,19 +45,22 @@
       </div>
 </template>
 
+<style scoped>
 
-<style>
-  .album-table {
+table {
     border-collapse: collapse;
     width: 100%;
     font-size: 14px;
   }
-  .album-table th,
-  .album-table td {
+
+  th, td {
     border: 1px solid black;
     padding: 0 6px;
     text-align: left;
   }
 
+  td:last-child {
+  padding: 0 3px;
+}
 
 </style>
