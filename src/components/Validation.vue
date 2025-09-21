9beaaf1c3f7ad1a6eb80ea3ea24743ab04e73f93
Kristina Diamond <krdiamond@gmail.com>
2025-09-20 23:42:47 -0400

    fixed width issue

--- src/components/Validation.vue
+++ src/components/Validation.vue
@@ -1,14 +1,16 @@
 <template>
-  <div>
-      <div class="close" @click="$emit('close')">X</div>
-        <h2 class="m-12">Buddha’s Parable of the Poisoned Arrow</h2>
-        <p class="m-12">
-          It's just as if <b>a man were wounded with an arrow</b> thickly smeared with poison. His friends &amp; companions, kinsmen &amp; relatives would provide him with a surgeon, and
-          <b>the man would say, 'I won't have this arrow removed until I know whether the man who wounded me was a noble warrior, a priest, a merchant, or a worker.'</b>
-          He would say, 'I won't have this arrow removed until I know the given name &amp; clan name of the man who wounded me... until I know whether he was tall, medium, or short... until I know whether he was dark, ruddy-brown, or golden-colored... until I know his home village, town, or city... until I know whether the bow with which I was wounded was a long bow or a crossbow... until I know whether the bowstring with which I was wounded was fiber, bamboo threads, sinew, hemp, or bark... until I know whether the shaft with which I was wounded was wild or cultivated... until I know whether the feathers of the shaft with which I was wounded were those of a vulture, a stork, a hawk, a peacock, or another bird... until I know whether the shaft with which I was wounded was bound with the sinew of an ox, a water buffalo, a langur, or a monkey.' He would say, 'I won't have this arrow removed until I know whether the shaft with which I was wounded was that of a common arrow, a curved arrow, a barbed, a calf-toothed, or an oleander arrow.'
-          <b>The man would die and those things would still remain unknown to him.</b>
-        </p>
-      </div>
+    <div>
+        <div class="wrapper">
+            <div class="close" @click="$emit('close')">X</div>
+            <h2 class="m-12">Buddha’s Parable of the Poisoned Arrow</h2>
+            <p class="m-12">
+              It's just as if <b>a man were wounded with an arrow</b> thickly smeared with poison. His friends &amp; companions, kinsmen &amp; relatives would provide him with a surgeon, and
+              <b>the man would say, 'I won't have this arrow removed until I know whether the man who wounded me was a noble warrior, a priest, a merchant, or a worker.'</b>
+              He would say, 'I won't have this arrow removed until I know the given name &amp; clan name of the man who wounded me... until I know whether he was tall, medium, or short... until I know whether he was dark, ruddy-brown, or golden-colored... until I know his home village, town, or city... until I know whether the bow with which I was wounded was a long bow or a crossbow... until I know whether the bowstring with which I was wounded was fiber, bamboo threads, sinew, hemp, or bark... until I know whether the shaft with which I was wounded was wild or cultivated... until I know whether the feathers of the shaft with which I was wounded were those of a vulture, a stork, a hawk, a peacock, or another bird... until I know whether the shaft with which I was wounded was bound with the sinew of an ox, a water buffalo, a langur, or a monkey.' He would say, 'I won't have this arrow removed until I know whether the shaft with which I was wounded was that of a common arrow, a curved arrow, a barbed, a calf-toothed, or an oleander arrow.'
+              <b>The man would die and those things would still remain unknown to him.</b>
+            </p>
+        </div>
+    </div>
 </template>
 
 
