40b822f600dca2bbc8a6b18fb98d7592342eb292
Kristina Diamond <krdiamond@gmail.com>
2025-09-21 08:49:50 -0400

    added X for close

--- src/components/Alchemy.vue
+++ src/components/Alchemy.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <div class="wrapper">
-<div class="close" @click="$emit('close')">X</div>
+    <div class="close" @click="$emit('close')">
+      <svg fill="#000000" width="800px" height="800px" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg">
+        <path d="M797.32 985.882 344.772 1438.43l188.561 188.562 452.549-452.549 452.548 452.549 188.562-188.562-452.549-452.548 452.549-452.549-188.562-188.561L985.882 797.32 533.333 344.772 344.772 533.333z"/>
+    </svg>
+</div>
         <h2 class="m-12">Alchemy</h2>
-        <p class="m-12">From the years 2023 to 2025 the Magnum Opus 🜍 was made manifest, revealed in its true form.  Daily the prima materia, freely bestowed, was set into the vessel. By my Stone it was transmuted, washed and perfected, until it shone as gold ☉ and with due solemnity deposited into my checking account in manifold measure. In August 2025, the month of my nativity, New Jersey's rulers forbade the Source, now barred in NJ, NY, and neighboring states. Yet the Work is eternal — TRUE ALCHEMY 🜔 will be discovered again.</p>
+        <p class="m-12">2023 to 2025: the Magnum Opus 🜍 was manifest, revealed in its true form. Daily the prima materia ◼︎ was set into the vessel ⚗. By my Stone it was transmuted, washed, perfected, and shone as gold ☉. With due solemnity it was laid into my checking account in manifold measure. In August 2025, the month of my nativity, New Jersey forbade the Source, and it was barred in NJ, NY, and neighboring states. Yet the Work endures, and TRUE ALCHEMY 🜔 will be found again.</p>
         <h4 class="m-12">꩜ node app - logs into "social casino", captures daily bonus token, plays "Genie" themed slot machine using token and "washes" it into real dollars, depositis dollars into my checking account.  The app no longer works due to NJ laws but here is a preview of my terminal output. </h4>
         <div class="m-12">
           <img :src="PassiveBaby" alt="Bird animation" class="w-full" />
