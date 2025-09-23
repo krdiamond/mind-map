2dbed1eda68842da63e9145779ec592c815d1e38
Kristina Diamond <krdiamond@gmail.com>
2025-09-22 20:28:08 -0400

    setting up paper and candle

--- src/App.vue
+++ src/App.vue
@@ -2,85 +2,123 @@
 
   
     <div class="h-250 sm-h-900 sm-p-20  relative flex justify-between align-bottom ">
+        <Validation v-if="showValidation" @close="toggleValidation" @click.stop v-draggable class="popup-box sm-w-400"/>
+        <Alchemy v-if="showAlchemy" @close="toggleAlchemy" @click.stop v-draggable class="popup-box sm-w-400"/>
+        <MusicPlayer v-if="showMusicPlayer" @close="toggleMusicPlayer" @click.stop v-draggable class="popup-box sm-w-400"/>
       
-      <TVStack ref="tvstack"/>    
+        <TVStack ref="tvstack"/>    
 
-      <Validation v-if="showValidation" @close="toggleValidation" @click.stop v-draggable class="popup-box sm-w-400"/>
-    <Alchemy v-if="showAlchemy" @close="toggleAlchemy" @click.stop v-draggable class="popup-box sm-w-400"/>
-    <MusicPlayer v-if="showMusicPlayer" @close="toggleMusicPlayer" @click.stop v-draggable class="popup-box sm-w-400"/>
-      <div class="cabinet-container h-250 sm-h-800 sm-mr-60 relative">
-        <img :src="Cabinet" class="h-250 sm-h-800 absolute" alt="Glass Cabinet" />
-          
-        <div id="Remote" class="remote absolute pointer" 
-              @click="onRemoteClick"
-              v-draggable="{
-              snapInto: [
-                  { left: 20, top: 700, right: 290, bottom: 720 }, // row 1
-                  { left: 20, top: 538, right: 290, bottom: 620 }, // row 2
-                  { left: 20, top: 410, right: 290, bottom: 420 }, // row 3
-                  { left: 20, top: 260, right: 290, bottom: 320 }, // row 4
-                  { left: 20, top: 114, right: 290, bottom: 120 }, // row 5
-                ],
-              snapDurationMs: 150,    
-              resetOnResize: true   
-            }"
-            alt="Remote">
-          <img :src="Remote" alt="Remote" class="remote" />
-          <click-spark style="--click-spark-color: black;"></click-spark>
-         </div>
+        <div class="cabinet-container h-250 sm-h-800 sm-mr-60 relative">
+          <img :src="Cabinet" class="h-250 sm-h-800 absolute" alt="Glass Cabinet" />
 
-         <div id="Buddha" @click="toggleValidation" class="buddha absolute pointer" 
+            <div id="Paper" class="paper" 
+                @click="onPaperClick"
                 v-draggable="{
                 snapInto: [
-                    { left: 40, top: 685, right: 278, bottom: 750}, // row 1
-                    { left: 40, top: 528, right: 278, bottom: 610 }, // row 2
-                    { left: 40, top: 387, right: 278, bottom: 420 }, // row 3
-                    { left: 40, top: 237, right: 278, bottom: 320 }, // row 4
-                    { left: 40, top: 93, right: 278, bottom: 120 }, // row 5
+                    { left: 20, top: 700, right: 290, bottom: 720 }, // row 1
+                    { left: 20, top: 538, right: 290, bottom: 620 }, // row 2
+                    { left: 20, top: 410, right: 290, bottom: 420 }, // row 3
+                    { left: 20, top: 260, right: 290, bottom: 320 }, // row 4
+                    { left: 20, top: 114, right: 290, bottom: 120 }, // row 5
                   ],
                 snapDurationMs: 150,    
                 resetOnResize: true   
               }"
-              alt="Buddha">
-            <img :src="Buddha" alt="Buddha" />
-         </div>
+              alt="Paper">
+            <!-- <img :src="Remote" alt="Candle" /> -->
+          </div>
 
 
-         <div id="AirpodPro" @click="toggleMusicPlayer" class="airpod-pro absolute pointer" 
+
+          <div id="Candle" class="candle" 
+                @click="onCandleClick"
                 v-draggable="{
                 snapInto: [
-                    { left: 40, top: 707, right: 278, bottom: 750}, // row 1
-                    { left: 40, top: 560, right: 278, bottom: 610 }, // row 2
-                    { left: 40, top: 413, right: 278, bottom: 420 }, // row 3
-                    { left: 40, top: 270, right: 278, bottom: 320 }, // row 4
-                    { left: 40, top: 128, right: 278, bottom: 120 }, // row 5
+                    { left: 20, top: 700, right: 290, bottom: 720 }, // row 1
+                    { left: 20, top: 538, right: 290, bottom: 620 }, // row 2
+                    { left: 20, top: 410, right: 290, bottom: 420 }, // row 3
+                    { left: 20, top: 260, right: 290, bottom: 320 }, // row 4
+                    { left: 20, top: 114, right: 290, bottom: 120 }, // row 5
                   ],
                 snapDurationMs: 150,    
                 resetOnResize: true   
               }"
-              alt="Airpod Pro">
-            <img :src="AirpodPro" alt="Airpod Pro" />
-         </div>
+              alt="Candle">
+            <!-- <img :src="Remote" alt="Candle" /> -->
+          </div>
 
-        <div id="Gold" @click="toggleAlchemy" class="gold absolute pointer" 
+
+            
+          <div id="Remote" class="remote" 
+                @click="onRemoteClick"
                 v-draggable="{
                 snapInto: [
-                    { left: 40, top: 711, right: 278, bottom: 711}, // row 1
-                    { left: 40, top: 560, right: 278, bottom: 560 }, // row 2
-                    { left: 40, top: 414, right: 278, bottom: 414 }, // row 3
-                    { left: 40, top: 268, right: 278, bottom: 268 }, // row 4
-                    { left: 40, top: 123, right: 278, bottom: 123 }, // row 5
+                    { left: 20, top: 700, right: 290, bottom: 720 }, // row 1
+                    { left: 20, top: 538, right: 290, bottom: 620 }, // row 2
+                    { left: 20, top: 410, right: 290, bottom: 420 }, // row 3
+                    { left: 20, top: 260, right: 290, bottom: 320 }, // row 4
+                    { left: 20, top: 114, right: 290, bottom: 120 }, // row 5
                   ],
                 snapDurationMs: 150,    
                 resetOnResize: true   
               }"
-              alt="Gold">
-            <img :src="Gold" alt="Gold" />
-         </div>
+              alt="Remote">
+            <img :src="Remote" alt="Remote" />
+            <click-spark style="--click-spark-color: black;"></click-spark>
+          </div>
 
-      </div>
+          <div id="Buddha" @click="toggleValidation" class="buddha" 
+                  v-draggable="{
+                  snapInto: [
+                      { left: 40, top: 685, right: 278, bottom: 750}, // row 1
+                      { left: 40, top: 528, right: 278, bottom: 610 }, // row 2
+                      { left: 40, top: 387, right: 278, bottom: 420 }, // row 3
+                      { left: 40, top: 237, right: 278, bottom: 320 }, // row 4
+                      { left: 40, top: 93, right: 278, bottom: 120 }, // row 5
+                    ],
+                  snapDurationMs: 150,    
+                  resetOnResize: true   
+                }"
+                alt="Buddha">
+              <img :src="Buddha" alt="Buddha" />
+          </div>
 
-  </div>
+
+          <div id="AirpodPro" @click="toggleMusicPlayer" class="airpod-pro" 
+                  v-draggable="{
+                  snapInto: [
+                      { left: 40, top: 707, right: 278, bottom: 750}, // row 1
+                      { left: 40, top: 560, right: 278, bottom: 610 }, // row 2
+                      { left: 40, top: 413, right: 278, bottom: 420 }, // row 3
+                      { left: 40, top: 270, right: 278, bottom: 320 }, // row 4
+                      { left: 40, top: 128, right: 278, bottom: 120 }, // row 5
+                    ],
+                  snapDurationMs: 150,    
+                  resetOnResize: true   
+                }"
+                alt="Airpod Pro">
+              <img :src="AirpodPro" alt="Airpod Pro" />
+          </div>
+
+          <div id="Gold" @click="toggleAlchemy" class="gold" 
+                  v-draggable="{
+                  snapInto: [
+                      { left: 40, top: 711, right: 278, bottom: 711}, // row 1
+                      { left: 40, top: 560, right: 278, bottom: 560 }, // row 2
+                      { left: 40, top: 414, right: 278, bottom: 414 }, // row 3
+                      { left: 40, top: 268, right: 278, bottom: 268 }, // row 4
+                      { left: 40, top: 123, right: 278, bottom: 123 }, // row 5
+                    ],
+                  snapDurationMs: 150,    
+                  resetOnResize: true   
+                }"
+                alt="Gold">
+              <img :src="Gold" alt="Gold" />
+          </div>
+
+        </div>
+
+    </div>
 </template>
 
 <script>
