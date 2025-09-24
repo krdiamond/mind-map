7a2268f96eaaa84fc2855602c5ee5e51f2e0e491
Kristina Diamond <krdiamond@gmail.com>
2025-09-24 12:17:42 -0400

    found a way to organize content

--- src/components/Gratitude.vue
+++ src/components/Gratitude.vue
@@ -6,10 +6,12 @@
                         <path d="M797.32 985.882 344.772 1438.43l188.561 188.562 452.549-452.549 452.548 452.549 188.562-188.562-452.549-452.548 452.549-452.549-188.562-188.561L985.882 797.32 533.333 344.772 344.772 533.333z"/>
                     </svg>
                 </div>
-            <h2 class="m-12">Gratitude Journal</h2>
-            <p class="m-12">
-              My eyes
-            </p>
+            <h2 class="m-12"><i>I am grateful for...</i></h2>
+            <ul>
+                <li>
+                    <b>9.23.2025</b> Buying the Jawbreaker tickets for 9/24 instead of 9/23 so I don't have to change my plans. Really oversaturated their demographic didn't they.
+                </li>
+            </ul>
         </div>
     </div>
 </template>
